17e189647b62823dc4979ed2f3296fc60647c82e
jaimebp07 <andrescodigo07@gmail.com>
2025-10-17 23:36:30 -0500

    🐛🚧 fixing 'Host is not specified'

--- applications/app-service/src/main/java/co/com/crediya/MainApplication.java
+++ applications/app-service/src/main/java/co/com/crediya/MainApplication.java
@@ -3,9 +3,14 @@ package co.com.crediya;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@ComponentScan(basePackages = {
+    "co.com.crediya", // Escaneo del paquete base y subpaquetes por si acaso
+    "co.com.crediya.consumer" // <-- Asegura que el adaptador sea escaneado
+})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
